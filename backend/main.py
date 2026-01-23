0caa4e6e6d1a39a3d2a04b93f0cb4a5944d20f8b
jakkayy <niti2042547@gmail.com>
2026-01-23 14:34:25 +0700

    add migrations

--- backend/main.py
+++ backend/main.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy import Integer, String
 from sqlalchemy.orm import Mapped, mapped_column
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 CORS(app)
@@ -14,6 +15,7 @@ class Base(DeclarativeBase):
     pass
 
 db = SQLAlchemy(app, model_class=Base)
+migrate=Migrate(app, db)
 class TodoItem(db.Model):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     title: Mapped[str] = mapped_column(String(100))
