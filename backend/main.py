c19fb83bd677b000850e91113a7c6b8223546a4e
jakkayy <niti2042547@gmail.com>
2026-01-23 14:54:10 +0700

    add miration in migrations/versions/

--- backend/main.py
+++ backend/main.py
@@ -2,8 +2,8 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy import Integer, String
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import Integer, String, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from flask_migrate import Migrate
 
 app = Flask(__name__)
@@ -20,17 +20,33 @@ class TodoItem(db.Model):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     title: Mapped[str] = mapped_column(String(100))
     done: Mapped[bool] = mapped_column(default=False)
+    
+    comments: Mapped[list["Comment"]] = relationship(back_populates="todo")
 
     def to_dict(self):
         return {
             "id": self.id,
             "title": self.title,
-            "done": self.done
+            "done": self.done,
+            "comments": [
+                comment.to_dict() for comment in self.comments
+            ]
         }
+class Comment(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    message: Mapped[str] = mapped_column(String(250))
+    todo_id: Mapped[int] = mapped_column(ForeignKey('todo_item.id'))
+    todo: Mapped["TodoItem"] = relationship(back_populates="comments")
     
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "message": self.message,
+            "todo_id": self.todo_id
+        }
         
-with app.app_context():
-    db.create_all()
+# with app.app_context():
+#     db.create_all()
 
 INITIAL_TODOS = [
     TodoItem(title='Learn Flask'),
