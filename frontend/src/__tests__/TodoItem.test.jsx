469e237299e4eab41605239232cfa6d751ee695b
jakkayy <niti2042547@gmail.com>
2026-02-13 15:24:01 +0700

    run test

---
+++ frontend/src/__tests__/TodoItem.test.jsx
@@ -0,0 +1,26 @@
+import { render, screen } from '@testing-library/react'
+import { expect } from 'vitest'
+import TodoItem from '../TodoItem.jsx'
+
+const baseTodo = {             // ** TodoItem พื้นฐานสำหรับทดสอบ
+  id: 1,
+  title: 'Sample Todo',
+  done: false,
+  comments: [],
+};
+
+describe('TodoItem', () => {
+  it('renders with no comments correctly', () => {    
+    const todoWithComment = {
+      ...baseTodo,
+      comments: [
+        {id: 1, message: 'First comment'},
+        {id: 2, message: 'Another comment'},
+      ]
+    };
+    render(
+      <TodoItem todo={todoWithComment} />
+    );
+    expect(screen.getByText('Sample Todo')).toBeInTheDocument();
+  });
+});
